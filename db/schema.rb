b16903661916305b9cf1357f3cc3508ee0e5dc61
revichung <s1492e034@gmail.com>
2018-09-12 21:18:24 +0800

    add contact_info to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_081423) do
+ActiveRecord::Schema.define(version: 2018_09_12_131711) do
 
   create_table "desserts", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_081423) do
     t.string "role", default: "general"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "contact_info"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

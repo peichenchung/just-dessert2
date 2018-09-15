5f7b9454ae6e98a003197addee112c33c4ba3c7e
revichung <s1492e034@gmail.com>
2018-09-15 19:06:59 +0800

    add route to order list for each dessert

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :desserts do
-    resources :orders, only: [:new, :create, :destroy]
+    resources :orders, only: [:new, :create, :destroy] do
+      member do
+        get :order_list
+      end
+    end
   end
 
   resources :users, only: [:show, :edit, :update]

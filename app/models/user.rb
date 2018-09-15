8a216baf63850b0701e8b46d48d40936bbbf6fbb
revichung <s1492e034@gmail.com>
2018-09-15 12:19:31 +0800

    mount avatar uploader to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates_presence_of :name
   has_many :desserts, dependent: :destroy
 
+  mount_uploader :avatar, AvatarUploader
+
   def admin?
     self.role == "admin"
   end

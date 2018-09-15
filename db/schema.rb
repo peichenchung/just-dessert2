669f7e710f9d55adda579b8720de154d9cd0c37a
revichung <s1492e034@gmail.com>
2018-09-15 18:50:50 +0800

    add sold_amount to desserts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_095604) do
+ActiveRecord::Schema.define(version: 2018_09_15_104956) do
 
   create_table "desserts", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_095604) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sold_amount"
   end
 
   create_table "orders", force: :cascade do |t|

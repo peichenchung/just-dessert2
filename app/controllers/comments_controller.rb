a15ccc9bcc0873d52b351c8ce9ca9d868bf470e5
Stanley <Stanley741025@gmail.com>
2018-09-16 19:27:24 +0800

    add delete action for admin user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
     @dessert = Dessert.find(params[:dessert_id])
     @comment = @dessert.comments.build(comment_params)
@@ -8,8 +9,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @dessert = Dessert.find(params[:dessert_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user.admin?
+      @comment.destroy
+      redirect_to dessert_path(@dessert)
+    end
+  end
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:content)
   end

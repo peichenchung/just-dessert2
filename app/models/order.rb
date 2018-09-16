1889f0e57645364a4176b2dc746a42ca1952f8ba
revichung <s1492e034@gmail.com>
2018-09-16 12:08:02 +0800

    order amount must be a number and must be integer

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   validates_presence_of :name, :phone, :amount #設為必填
+  validates :amount, numericality: { only_integer: true } #只能填數字，且必須是整數
   belongs_to :user
   belongs_to :dessert
   belongs_to :seller, class_name: "User"

7c15bf77d5d0a395c0f22129f7e409e6b6760d3e
revichung <s1492e034@gmail.com>
2018-09-16 10:55:11 +0800

    buyer can leave a message to seller when place an order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_141236) do
+ActiveRecord::Schema.define(version: 2018_09_16_024457) do
 
   create_table "desserts", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_141236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "seller_id"
+    t.text "message"
   end
 
   create_table "users", force: :cascade do |t|

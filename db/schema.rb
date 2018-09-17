c71da52bea8f144ef89c97083904d7226dc5c86c
revichung <s1492e034@gmail.com>
2018-09-17 23:18:00 +0800

    add uid and provider columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_015513) do
+ActiveRecord::Schema.define(version: 2018_09_17_151716) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2018_09_17_015513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "contact_info"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

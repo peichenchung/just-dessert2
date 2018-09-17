373b269c69be75ecb04b7e3cf227f5c563d516f3
revichung <s1492e034@gmail.com>
2018-09-17 11:34:27 +0800

    everyone can see other's crowdfundings from other's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @desserts = current_user.desserts
+    @desserts = @user.desserts
     @orders = current_user.orders
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2018_09_16_103647) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "dessert_id"
-    t.integer "seller_id"
     t.integer "user_id"
     t.string "name"
     t.string "phone"
@@ -50,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_09_16_103647) do
     t.integer "sn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seller_id"
     t.text "message"
   end
 

ecece22355650d39192ca32c660cbc335c8d7cd7
revichung <s1492e034@gmail.com>
2018-09-20 23:18:29 +0800

    cancel validates_presence_of image in dessert model

--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -1,6 +1,6 @@
 class Dessert < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates_presence_of :name, :image, :price, :amount, :location #設為必填
+  validates_presence_of :name, :price, :amount, :location #設為必填
   belongs_to :user #seller
   has_many :orders, dependent: :destroy
   has_many :comments, dependent: :destroy

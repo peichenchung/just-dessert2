423f16f750437a8513c06c10b1d703e0120c334e
Stanley <Stanley741025@gmail.com>
2018-09-21 11:31:39 +0800

    setup category resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
+
+
   resources :desserts do
     resources :orders
     resources :comments, only: [:create, :destroy] do
       resources :replies, only: [:create, :destroy]
     end
   end
-
+  
+  resources :categories, only: :show
   resources :users, only: [:show, :edit, :update]
   resources :orders
   root "desserts#index"

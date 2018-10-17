fc41709138e418c465c33d0181c504b5fd6c9ec0
revichung <s1492e034@gmail.com>
2018-10-17 15:47:57 +0800

    change new aws access keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'AKIAJL4K4SJYX3N7QUBQ',
-      aws_secret_access_key: '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',
+      aws_access_key_id:     'AKIAJ6INWAZTV64RLGPQ',
+      aws_secret_access_key: '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',
       region:                'ap-northeast-1'
     }
     config.fog_directory  = 'just-dessert2'

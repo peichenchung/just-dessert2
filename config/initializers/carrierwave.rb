2af002182e406c9b8520141582e81f597176f802
revichung <s1492e034@gmail.com>
2018-10-17 16:04:53 +0800

    setting carrierwave upload to aws s3 again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'AKIAJ6INWAZTV64RLGPQ',
-      aws_secret_access_key: '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',
+      aws_access_key_id:     ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET'],
       region:                'ap-northeast-1'
     }
-    config.fog_directory  = 'just-dessert2'
+    config.fog_directory  = ENV['S3_BUCKET']
   else
     config.storage :file
   end

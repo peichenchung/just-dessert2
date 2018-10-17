43ed4082645ee04992ae5f3d63cffcca1ed7245b
revichung <s1492e034@gmail.com>
2018-10-17 15:36:30 +0800

    update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
+      aws_access_key_id:     'AKIAJL4K4SJYX3N7QUBQ',
+      aws_secret_access_key: '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',
       region:                'ap-northeast-1'
     }
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = 'just-dessert2'
   else
     config.storage :file
   end

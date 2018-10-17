5f08ee6d9b4bb6937bd60575338b0f53fb2e6e8e
revichung <s1492e034@gmail.com>
2018-10-17 14:47:53 +0800

    modify uploader settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws' 
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV["aws_access_key_id"],
-    aws_secret_access_key: ENV["aws_secret_access_key"],
-    host:                  'http://s3-ap-northeast-1.amazonaws.com',
-    region:                'ap-northeast-1'
-  }
-  config.fog_directory  = ENV["fog_directory"]
+  config.fog_provider = 'fog/aws'
+  if Rails.env.production?
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["aws_access_key_id"],
+      aws_secret_access_key: ENV["aws_secret_access_key"],
+      host:                  'http://s3-ap-northeast-1.amazonaws.com',
+      region:                'ap-northeast-1'
+    }
+    config.fog_directory  = ENV["fog_directory"]
+  else
+    config.storage :file
+  end
 end

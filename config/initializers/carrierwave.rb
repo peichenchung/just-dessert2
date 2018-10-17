1355a0f440b763d375fd9b2f4735610c7da08599
revichung <s1492e034@gmail.com>
2018-10-17 14:12:37 +0800

    fix settings of uploader to aws s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
+      aws_access_key_id:     ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
+      host:                  'http://s3-ap-northeast-1.amazonaws.com',
       region:                'ap-northeast-1'
     }
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = ENV['fog_directory']
   else
     config.storage :file
   end

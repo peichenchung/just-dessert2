1e86d1b175af20ed516b86c7dc2debef9eb15649
revichung <s1492e034@gmail.com>
2018-10-19 16:43:34 +0800

    extract spgateway config to yml

--- lib/spgateway.rb
+++ lib/spgateway.rb
@@ -6,10 +6,13 @@ class Spgateway
 
   def initialize(payment)
     @payment = payment
-    self.merchant_id = "MS35081680"
-    self.hash_key = "zkn80MmVP0h9sC4V3eyN54W0QxFUYNxP"
-    self.hash_iv = "du0Zeee6uaFZGZuL"
-    self.url = "https://ccore.spgateway.com/MPG/mpg_gateway"
+
+    spgateway_config = Rails.application.config_for(:spgateway)
+
+    self.merchant_id = spgateway_config["merchant_id"]
+    self.hash_key = spgateway_config["hash_key"]
+    self.hash_iv = spgateway_config["hash_iv"]
+    self.url = spgateway_config["url"]
   end
 
   def generate_form_data(return_url)
